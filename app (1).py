49471f4b5ff96e40f95ee79af66863c72bbb258e
Amarpal-singh-dutta <amarpal.12222132@lpu.in>
2026-04-16 18:29:52 +0530

    Add files via upload

---
+++ app (1).py
@@ -0,0 +1,78 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+# load trained pipeline
+model = joblib.load("model.pkl")
+
+st.title("Customer Churn Prediction")
+
+st.subheader("Enter Customer Details")
+
+# -------- INPUTS -------- #
+
+gender = st.selectbox("Gender", ["Male", "Female"])
+SeniorCitizen = st.selectbox("Senior Citizen", [0, 1])
+Partner = st.selectbox("Partner", ["Yes", "No"])
+Dependents = st.selectbox("Dependents", ["Yes", "No"])
+
+tenure = st.number_input("Tenure (months)", min_value=0)
+
+PhoneService = st.selectbox("Phone Service", ["Yes", "No"])
+MultipleLines = st.selectbox("Multiple Lines", ["Yes", "No", "No phone service"])
+
+InternetService = st.selectbox("Internet Service", ["DSL", "Fiber optic", "No"])
+
+OnlineSecurity = st.selectbox("Online Security", ["Yes", "No", "No internet service"])
+OnlineBackup = st.selectbox("Online Backup", ["Yes", "No", "No internet service"])
+DeviceProtection = st.selectbox("Device Protection", ["Yes", "No", "No internet service"])
+TechSupport = st.selectbox("Tech Support", ["Yes", "No", "No internet service"])
+
+StreamingTV = st.selectbox("Streaming TV", ["Yes", "No", "No internet service"])
+StreamingMovies = st.selectbox("Streaming Movies", ["Yes", "No", "No internet service"])
+
+Contract = st.selectbox("Contract", ["Month-to-month", "One year", "Two year"])
+PaperlessBilling = st.selectbox("Paperless Billing", ["Yes", "No"])
+
+PaymentMethod = st.selectbox("Payment Method", [
+    "Electronic check",
+    "Mailed check",
+    "Bank transfer (automatic)",
+    "Credit card (automatic)"
+])
+
+MonthlyCharges = st.number_input("Monthly Charges")
+TotalCharges = st.number_input("Total Charges")
+
+# -------- PREDICTION -------- #
+
+if st.button("Predict"):
+
+    input_data = pd.DataFrame([{
+        'gender': gender,
+        'SeniorCitizen': SeniorCitizen,
+        'Partner': Partner,
+        'Dependents': Dependents,
+        'tenure': tenure,
+        'PhoneService': PhoneService,
+        'MultipleLines': MultipleLines,
+        'InternetService': InternetService,
+        'OnlineSecurity': OnlineSecurity,
+        'OnlineBackup': OnlineBackup,
+        'DeviceProtection': DeviceProtection,
+        'TechSupport': TechSupport,
+        'StreamingTV': StreamingTV,
+        'StreamingMovies': StreamingMovies,
+        'Contract': Contract,
+        'PaperlessBilling': PaperlessBilling,
+        'PaymentMethod': PaymentMethod,
+        'MonthlyCharges': MonthlyCharges,
+        'TotalCharges': TotalCharges
+    }])
+
+    prediction = model.predict(input_data)[0]
+
+    if prediction == 1:
+        st.error("⚠️ Customer is likely to CHURN")
+    else:
+        st.success("✅ Customer is NOT likely to churn")
